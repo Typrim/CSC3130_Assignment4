f6b7446c7005528defe626b5041fbae51a586ae5
Typrim <tommynguyen0531@gmail.com>
2026-03-12 11:43:39 -0500

    Completed Problem3, Started Problem6

---
+++ src/Problem6.java
@@ -0,0 +1,11 @@
+public class Problem6 {
+    public static void main(String[] args) {
+        String input = "a+b*(c^d-e)^(f+g*h)-i";
+    }
+
+    public String convertInfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+
+        return postfix.toString();
+    }
+}
