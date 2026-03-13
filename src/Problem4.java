b678619ebdd73062a791a4b92abe8c795e18061e
Typrim <tommynguyen0531@gmail.com>
2026-03-12 23:32:28 -0500

    Removed Map implementation in Problem4

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 public class Problem4 {
@@ -16,29 +14,31 @@ public class Problem4 {
         System.out.println(instance.isBalanced(test3));
     }
 
-    private Map<Character, Character> allowedBrackets = new HashMap<>();
+    private static final String FIRST_BRACKETS = "{([<¿";
+    private static final String SECOND_BRACKETS = "})]>?";
 
-    public Problem4() {
-        allowedBrackets.put('{', '}');
-        allowedBrackets.put('(', ')');
-        allowedBrackets.put('[', ']');
-        allowedBrackets.put('<', '>');
-        allowedBrackets.put('¿', '?');
-    }
+    public Problem4() {}
 
     public String isBalanced(String s) {
-        char[] inputArray = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        for (char character : inputArray) {
-            if (!allowedBrackets.containsKey(character) && !allowedBrackets.containsValue(character)) {
-                throw new IllegalArgumentException("Does not contain only acceptable bracket characters.");
-            }
-            if (allowedBrackets.containsKey(character)) {
-                stack.push(allowedBrackets.get(character));
-            } else if (stack.pop() != character) {
-                return "NO";
+        for (char character : s.toCharArray()) {
+            if (FIRST_BRACKETS.contains(String.valueOf(character))) {
+                stack.push(character);
+            } else if (SECOND_BRACKETS.contains(String.valueOf(character))) {
+                if (returnPosition(stack.pop(), FIRST_BRACKETS.toCharArray()) != returnPosition(character, SECOND_BRACKETS.toCharArray())) {
+                    return "NO";
+                }
             }
         }
         return "YES";
+    }
+
+    private int returnPosition(char character, char[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (character == array[index]) {
+                return index;
+            }
+        }
+        return -1;
     }
 }
--- src/Problem6.java
+++ src/Problem6.java
@@ -1,10 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Problem6 {
     public static void main(String[] args) {
         String input = "a+b*(c^d-e)^(f+g*h)-i";
     }
 
     public String convertInfix(String infix) {
+        //holds postfix
         StringBuilder postfix = new StringBuilder();
+
+        //convert infix
+        char[] infixCharacters = infix.toCharArray();
+        Queue<Character> queue = new LinkedList<>();
+        for (char character : infixCharacters) {
+            queue.add(character);
+        }
+        while (!queue.isEmpty()) {
+            char character = queue.remove();
+            //prioritize operations within parentheses
+
+            //prioritize ^ operations, ignoring right associativity
+
+        }
+
 
         return postfix.toString();
     }
